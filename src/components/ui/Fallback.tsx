e5037334b7426b1fcdc3509fa8d13370beb9d349
IraiDevPersonal <ignacio.arr01@gmail.com>
2023-02-05 17:09:51 -0300

    cambios a modo oscuro y fallback suspense

--- src/components/ui/Fallback.tsx
+++ src/components/ui/Fallback.tsx
@@ -2,7 +2,7 @@ import { BiLoaderAlt } from 'react-icons/bi'
 
 export function Fallback() {
    return (
-      <main className="h-screen bg-slate-800 text-slate-300 grid place-content-center">
+      <main className="h-screen dark:bg-slate-800 bg-slate-50 dark:text-slate-300 text-slate-800 grid place-content-center">
          <div>
             <BiLoaderAlt className="text-5xl animate-spin" />
          </div>
